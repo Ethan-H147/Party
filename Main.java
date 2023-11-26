b3f3fe5cfde714bb766e0d974b948a6f5fa7d6e4
Ethan-H147 <97569719+Ethan-H147@users.noreply.github.com>
2023-11-26 11:34:19 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,9 +3,22 @@ import java.io.FileNotFoundException;  // Import this class to handle errors
 // Import the Scanner class to read text files
 import java.util.*;
 
+
+/**
+ * Ethan Hu
+ * 2023/11/26
+ * Party Planner
+ * Main Class-core of the program, controlling the basic running logic
+ */
+
+
+  /*
+  central logic of the program
+  First import the text document to create a list of attendees
+  then add all the function to a while loop, keep running until the user input quit
+ */
 public class Main{
-	
-	
+		
 	public static void main (String[] args){  
 	Party p1 = new Party();
 	ArrayList<Attendee> party = new ArrayList<Attendee>();
@@ -17,7 +30,7 @@ public class Main{
 	String[] companyNames = {"WalMart","Kroger","Amazon","Lowes","Best Western","KMart","Fusian","Heinz","Gucci","Prada","Nike","Dodge","Maserati","Razor","AMD","Razer"};
 	String[] myArray;
   int totalTable = 10;
-  ArrayList<ArrayList<Attendee>> tables = new ArrayList<>();
+  ArrayList<ArrayList<Attendee>> tables = new ArrayList<>();//2D ArrayList, arraylist of arraylists
 		
 		try {
       File myObj = new File("partyguests.txt");
@@ -26,7 +39,7 @@ public class Main{
         String data = myReader.nextLine();
         //System.out.println(data);
         myArray = data.split(",");
-        id = Integer.parseInt(myArray[0]);
+        id = Integer.parseInt(myArray[0]);//split the info into four parts to create attendee
         lName = myArray[1];
         fName = myArray[2];
         comp = Integer.parseInt(myArray[3]);
@@ -42,8 +55,8 @@ public class Main{
 
     if(!p1.partyCheck(party)){
       System.out.println("Company member limit or party limit exceeded. Unable to assign tables.");
-    }
-    String function = "x";
+    }//starting check for the list
+    String function = "";
     do{
     System.out.print("What do you want to do?(Search/Add/Assign/Table/Company Search/Quit)");
     function= scan.nextLine();
@@ -53,18 +66,19 @@ public class Main{
     if(function.equalsIgnoreCase("search")) {
             System.out.print("Enter first name or last name: ");
             String searchName = scan.nextLine();
-            if(p1.searchAttendee(party, searchName)==-1){
+            if(p1.searchAttendee(party, searchName)==-1){//doesn't exist
               System.out.print("Attendee doesn't exist.\n");
             }
             else{
             System.out.print(p1.searchAttendee(party, searchName)+". "+party.get(p1.searchAttendee(party, searchName)-1).getfName()+" "+party.get(p1.searchAttendee(party, searchName)-1).getlName());
             System.out.println(" / "+companyNames[party.get(p1.searchAttendee(party, searchName)-1).getComp()-1]);
-            for(int i=0; i<tables.size(); i++) {
+            for(int i=0; i<tables.size(); i++) {//go through the tables
               ArrayList<Attendee> currentTable=tables.get(i);
-              for (int j=0; j<currentTable.size(); j++) {
+              for (int j=0; j<currentTable.size(); j++) {//go through the seats
                   Attendee attendee = currentTable.get(j);
+                  //if the name matches entirely, print the table and seat number of that person
                   if (attendee.getfName().equalsIgnoreCase(party.get(p1.searchAttendee(party, searchName)-1).getfName()) && attendee.getlName().equalsIgnoreCase(party.get(p1.searchAttendee(party, searchName)-1).getlName())) {
-                      System.out.println("Table "+(i+1)+" Seat "+(j+1));
+                      System.out.println("Table "+(i+1)+" Seat "+(j+1));//print the table and seat
                   }
                 }
                 }
@@ -74,10 +88,13 @@ public class Main{
       if(function.equalsIgnoreCase("add")) {
             System.out.print("Enter first name and last name: ");
             p1.addAttendee(party);
+            if(!p1.partyCheck(party)){//adding attendee might cause the party to exceed its limits, I didn't make it stop adding attendees, but the assigning function surely won't work
+                System.out.println("Company member limit or party limit exceeded. Unable to assign tables.");
+            }//check after new member is added
         }
 
       if(function.equalsIgnoreCase("assign")) {
-        tables = p1.assignTables(party);
+        tables = p1.assignTables(party);//just assigning, print function is in "table"
           /*for(int i = 0;i<totalTable;i++){
             System.out.println(tables.get(i));
           }*/
@@ -87,7 +104,7 @@ public class Main{
         if(function.equalsIgnoreCase("table")) {
           System.out.print("Enter the table number: ");
           int table = scan.nextInt();
-          if(tables.size()>=table){
+          if(tables.size()>=table){//check if it's printable
           System.out.println(tables.get(table-1));
           }
           else{System.out.println("Table not assigned yet.");}
@@ -98,10 +115,8 @@ public class Main{
           int company = scan.nextInt();
           p1.printCompany(party, company);
         }
-        
       }
-      while(!function.equalsIgnoreCase("Quit"));
+      while(!function.equalsIgnoreCase("Quit"));//stop the program
       //System.out.print(party);
 	}
 }
-
