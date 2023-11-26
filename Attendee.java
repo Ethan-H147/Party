7a84a71f86fa7fd779e5d8bf78837e8a066f92b3
Ethan-H147 <97569719+Ethan-H147@users.noreply.github.com>
2023-11-26 11:33:52 -0500

    Update Attendee.java

--- Attendee.java
+++ Attendee.java
@@ -3,6 +3,18 @@ import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
 import java.util.*;
 
+/**
+ * Ethan Hu
+ * 2023/11/26
+ * Party Planner
+ * Attendee Class-everything about an individual, the attributes, the way of printing that attendee
+ */
+
+/*
+ * This is the attendee class
+ * it includes the attributes of an attendee
+ * it also has getters and setter, along with a to string method to print attendees 
+ */
 public class Attendee {
 	private int ID;
 	private String fName;
@@ -13,6 +25,10 @@ public class Attendee {
 	String[] companyNames = {"WalMart","Kroger","Amazon","Lowes","Best Western","KMart","Fusian","Heinz","Gucci","Prada","Nike","Dodge","Maserati","Razor","AMD","Razer"};
 
 
+	/*
+	 * Setter, create an attendee with 4 attributes
+	 * company is the string for the actual name of the company, decided by the int comp
+	 */
 	public Attendee(int initID, String initlName, String initfName, int initComp){
 		ID = initID;
 		fName = initfName;
@@ -22,19 +38,21 @@ public class Attendee {
 	}
 	
 	
-	
+	//toString method for printing
 	public String toString(){
 			return (ID+" "+fName+" "+lName+" "+comp);
 		}
 		
 	
-	
+	//getter for last name
 	public String getlName(){
 		return lName;
 	}
+	//getter for first name
 	public String getfName(){
 		return fName;
 	}
+	//getter for company
 	public int getComp(){
 		return comp;
 	}
